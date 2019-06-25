8a4ec25388b12c9b87c93fbf2291c21102a0cf58
zyw <40329182+zhangyongwang4444@users.noreply.github.com>
2019-06-25 10:39:28 +0800

    修复CountingHashSet实现中的bug (#3)
    修复CountingHashSet实现中的bug (#3)

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -5,7 +5,9 @@ import java.util.Collection;
 import java.util.HashSet;
 
 public class CountingSet extends HashSet<Object> {
-    /** 统计"有史以来"向该集合中添加过的元素个数 */
+    /**
+     * 统计"有史以来"向该集合中添加过的元素个数
+     */
     private int count = 0;
 
     @Override
@@ -14,11 +16,7 @@ public class CountingSet extends HashSet<Object> {
         return super.add(obj);
     }
 
-    @Override
-    public boolean addAll(Collection c) {
-        count += c.size();
-        return super.addAll(c);
-    }
+
 
     public int getCount() {
         return count;
