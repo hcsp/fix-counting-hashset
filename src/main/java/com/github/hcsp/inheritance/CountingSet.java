76b7601d4beddafff6fcba20ff304d9888db7e11
hukaibo <52575259+hukaibo@users.noreply.github.com>
2019-08-01 08:38:16 +0800

    修复CountingSet实现中的bug (#29)
    修复CountingSet实现中的bug (#29)

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -14,11 +14,11 @@ public class CountingSet extends HashSet<Object> {
         return super.add(obj);
     }
 
-    @Override
-    public boolean addAll(Collection c) {
-        count += c.size();
-        return super.addAll(c);
-    }
+//    @Override
+//    public boolean addAll(Collection c) {
+//        count += c.size();
+//        return super.addAll(c);
+//    }
 
     public int getCount() {
         return count;
