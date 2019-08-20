a73c1e387f31ff0e1dfe32f687115808301fcbb1
Suyu <zhuangsuyu1993@163.com>
2019-08-20 17:16:05 +0800

    统计"有史以来"添加到其中去的元素个数 (#37)
    统计"有史以来"添加到其中去的元素个数 (#37)

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -10,13 +10,14 @@ public class CountingSet extends HashSet<Object> {
 
     @Override
     public boolean add(Object obj) {
-        count++;
+        if (obj != null) {
+            count++;
+        }
         return super.add(obj);
     }
 
     @Override
     public boolean addAll(Collection c) {
-        count += c.size();
         return super.addAll(c);
     }
 
