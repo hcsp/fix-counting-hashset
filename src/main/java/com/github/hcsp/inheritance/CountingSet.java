02dfbaee693f23e7edd690a53935ba4bd13aff2b
qiaomengnan <2568151682@qq.com>
2019-10-03 17:30:19 +0800

    count

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -16,7 +16,7 @@ public class CountingSet extends HashSet<Object> {
 
     @Override
     public boolean addAll(Collection c) {
-        count += c.size();
+       // count += c.size();
         return super.addAll(c);
     }
 
