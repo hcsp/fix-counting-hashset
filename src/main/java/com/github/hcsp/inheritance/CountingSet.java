6cb0169c39d98a604edc74344feca3de62252ad5
emm <3108775859@qq.com>
2019-10-25 01:11:24 +0800

    为什么这样写不可以

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -4,32 +4,22 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
-public class CountingSet{
+public class CountingSet extends HashSet{
 
     private HashSet<Object> set = new HashSet<>();
     /** 统计"有史以来"向该集合中添加过的元素个数 */
     private int count = 0;
 
+    @Override
     public boolean add(Object obj) {
         count++;
         return set.add(obj);
     }
 
+    @Override
     public boolean addAll(Collection c) {
         count += c.size();
         return set.addAll(c);
-    }
-
-    public int size() {
-        return set.size();
-    }
-
-    public boolean removeAll(Collection c) {
-        return set.removeAll(c);
-    }
-
-    public boolean remove(Object o) {
-        return set.remove(o);
     }
 
     public int getCount() {
