7b51119b5f7c1c1325ad5f2fde8259115c6dfc30
Ke Rui <31921365+kerrypnx@users.noreply.github.com>
2019-11-28 16:57:59 +0800

    fix counting hashset (#85)
    fix counting hashset (#85)

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -4,20 +4,29 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
-public class CountingSet extends HashSet<Object> {
+public class CountingSet  {
     /** 统计"有史以来"向该集合中添加过的元素个数 */
     private int count = 0;
 
-    @Override
+    HashSet<Object> hashSet = new HashSet<>();
     public boolean add(Object obj) {
         count++;
-        return super.add(obj);
+        return hashSet.add(obj);
     }
+    public boolean remove(Object obj){
+        return hashSet.remove(obj);
+    }
+    public boolean removeAll(Collection c){
+        return hashSet.removeAll(c);
 
-    @Override
+    }
     public boolean addAll(Collection c) {
         count += c.size();
-        return super.addAll(c);
+        return hashSet.addAll(c);
+    }
+
+    public int size(){
+        return hashSet.size();
     }
 
     public int getCount() {
@@ -31,7 +40,7 @@ public class CountingSet extends HashSet<Object> {
         CountingSet countingSet = new CountingSet();
         countingSet.add(new Object());
         countingSet.addAll(Arrays.asList(1, 2, 3));
-
         System.out.println(countingSet.getCount());
     }
+
 }
