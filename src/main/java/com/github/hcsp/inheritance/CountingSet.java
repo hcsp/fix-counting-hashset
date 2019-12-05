0f9e9cd6fa741087dfacaddae32dcb379623c1d4
Administrator <2433005622@qq.com>
2019-12-05 17:31:44 +0800

    修复CountingSet实现中的bug。

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -4,29 +4,44 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
-public class CountingSet extends HashSet<Object> {
-    /** 统计"有史以来"向该集合中添加过的元素个数 */
+public class CountingSet {
+    HashSet<Object> hashSet = new HashSet<>();
+    /**
+     * 统计"有史以来"向该集合中添加过的元素个数
+     */
     private int count = 0;
 
-    @Override
     public boolean add(Object obj) {
         count++;
-        return super.add(obj);
+        return hashSet.add(obj);
     }
 
-    @Override
     public boolean addAll(Collection c) {
         count += c.size();
-        return super.addAll(c);
+        return hashSet.addAll(c);
     }
+
 
     public int getCount() {
         return count;
     }
 
+    public int size() {
+        return hashSet.size();
+    }
+
+    public boolean remove(Object o) {
+        return hashSet.remove(o);
+    }
+
+    public boolean removeAll(Collection c) {
+        return hashSet.removeAll(c);
+    }
     // 我们希望创建一个Set，能够统计"有史以来"添加到其中去的元素个数
     // 但是，现在结果明显不对
+
     // 请尝试修复此问题
+
     public static void main(String[] args) {
         CountingSet countingSet = new CountingSet();
         countingSet.add(new Object());
