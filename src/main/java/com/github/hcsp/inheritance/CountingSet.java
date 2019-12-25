4842ca6890d07e40e451a0562ff31d0ed01f841e
Jane Wang <janewangcoa@gmail.com>
2019-12-25 19:37:28 +0800

    修复CountingSet

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -6,7 +6,8 @@ import java.util.HashSet;
 
 public class CountingSet extends HashSet<Object> {
     /** 统计"有史以来"向该集合中添加过的元素个数 */
-    private int count = 0;
+    HashSet<Object> set = new HashSet<>();
+    private static int count = 0;
 
     @Override
     public boolean add(Object obj) {
@@ -16,9 +17,9 @@ public class CountingSet extends HashSet<Object> {
 
     @Override
     public boolean addAll(Collection c) {
-        count += c.size();
         return super.addAll(c);
     }
+
 
     public int getCount() {
         return count;
