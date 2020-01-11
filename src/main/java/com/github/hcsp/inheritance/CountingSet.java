dbaacd12fe3d8986c936bdc101d794a374221f9f
zml59 <59412107+zml59@users.noreply.github.com>
2020-01-11 23:59:33 +0800

    选用组合避免继承重写导致的方法bug,加上泛型。 (#115)
    选用组合避免继承重写导致的方法bug,加上泛型。 (#115)

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
-public class CountingSet extends HashSet<Object> {
-    /** 统计"有史以来"向该集合中添加过的元素个数 */
+public class CountingSet {
+    /**统计"有史以来"向该集合中添加过的元素个数*/
+    HashSet<Object> set = new HashSet<Object>();
+
     private int count = 0;
 
-    @Override
     public boolean add(Object obj) {
         count++;
-        return super.add(obj);
+        return set.add(obj);
     }
 
-    @Override
     public boolean addAll(Collection c) {
         count += c.size();
-        return super.addAll(c);
+        return set.addAll(c);
     }
 
     public int getCount() {
@@ -33,5 +33,17 @@ public class CountingSet extends HashSet<Object> {
         countingSet.addAll(Arrays.asList(1, 2, 3));
 
         System.out.println(countingSet.getCount());
+    }
+
+    public boolean removeAll(Collection obj) {
+        return set.removeAll(obj);
+    }
+
+    public int size() {
+        return set.size();
+    }
+
+    public boolean remove(Object o) {
+        return set.remove(o);
     }
 }
