9d2deba106593493965bf4c9c7c19a25d166d7f8
breeze <1023349760@qq.com>
2020-03-01 19:00:37 +0800

    完成添加计数

---
+++ src/main/java/com/github/hcsp/inheritance/Main.java
@@ -0,0 +1,22 @@
+package com.github.hcsp.inheritance;
+
+public class Main {
+    class Driver{
+        void drive(){
+
+        }
+
+        void fix(){
+
+        }
+    }
+
+    class Doctor {
+        void zhibing(){
+
+        }
+    }
+
+
+
+}
