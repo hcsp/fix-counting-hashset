8936c6c685c26461977025488522a2a2ea59f990
chenxi1916 <60290522+chenxi1916@users.noreply.github.com>
2020-03-18 01:50:44 +0800

    继承和组合的区别 (#149)
    继承和组合的区别 (#149)

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -4,24 +4,35 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
-public class CountingSet extends HashSet<Object> {
+public class CountingSet {
+    HashSet<Object> set = new HashSet<>( );
     /** 统计"有史以来"向该集合中添加过的元素个数 */
     private int count = 0;
 
-    @Override
     public boolean add(Object obj) {
         count++;
-        return super.add(obj);
+        return set.add(obj);
     }
 
-    @Override
     public boolean addAll(Collection c) {
         count += c.size();
-        return super.addAll(c);
+        return set.addAll(c);
     }
 
     public int getCount() {
         return count;
+    }
+
+    public boolean remove(Object obj) {
+        return set.remove(obj);
+    }
+
+    public boolean removeAll(Collection c) {
+        return set.removeAll(c);
+    }
+
+    public int size() {
+        return set.size();
     }
 
     // 我们希望创建一个Set，能够统计"有史以来"添加到其中去的元素个数
