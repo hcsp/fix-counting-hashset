7dbc1cdd10620afc0afc023ba59a8bcaa3ecfa1d
yujie-zhang <907420318@qq.com>
2020-05-15 16:53:55 +0800

    修复CountingSet实现中的bug

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -5,20 +5,29 @@ import java.util.Collection;
 import java.util.HashSet;
 
 public class CountingSet {
-    HashSet<Object> set = new HashSet<>();
+    private HashSet<Object> set = new HashSet<>();
     /** 统计"有史以来"向该集合中添加过的元素个数 */
     private int count = 0;
 
-//    @Override
     public boolean add(Object obj) {
         count++;
         return set.add(obj);
     }
 
-//    @Override
     public boolean addAll(Collection c) {
         count += c.size();
         return set.addAll(c);
+    }
+    public void remove(Object object) {
+        set.remove(object);
+    }
+
+    public boolean removeAll(Collection collection) {
+        return set.removeAll(collection);
+    }
+
+    public int size() {
+        return set.size();
     }
 
     public int getCount() {
