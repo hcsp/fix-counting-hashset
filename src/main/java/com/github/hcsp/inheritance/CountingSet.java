be5ec86881c30bd5b318c7b02cefd894d7a117c3
陳雲濤 <36143626+0x522@users.noreply.github.com>
2020-06-27 14:07:50 +0800

    修复CountingSet类中的bug。 (#186)
    修复CountingSet类中的bug。 (#186)

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -4,20 +4,24 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
-public class CountingSet extends HashSet<Object> {
-    /** 统计"有史以来"向该集合中添加过的元素个数 */
-    private int count = 0;
 
-    @Override
+public class CountingSet {
+    /**
+     * 统计"有史以来"向该集合中添加过的元素个数
+     */
+    private int count = 0;
+    HashSet<Object> set = new HashSet<>();
+
+    //    @Override
     public boolean add(Object obj) {
         count++;
-        return super.add(obj);
+        return set.add(obj);
     }
 
-    @Override
+    //    @Override
     public boolean addAll(Collection c) {
         count += c.size();
-        return super.addAll(c);
+        return set.addAll(c);
     }
 
     public int getCount() {
@@ -33,5 +37,17 @@ public class CountingSet extends HashSet<Object> {
         countingSet.addAll(Arrays.asList(1, 2, 3));
 
         System.out.println(countingSet.getCount());
+    }
+
+    public void remove(int i) {
+        set.remove(i);
+    }
+
+    public int size() {
+        return set.size();
+    }
+
+    public void removeAll(Collection c) {
+        set.removeAll(c);
     }
 }
