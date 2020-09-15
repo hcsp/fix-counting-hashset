7f164dbcedaa0af196f8f904684a4349eaf432d0
zhu <z823511280>
2020-09-16 00:26:46 +0800

    修复CountingSet实现中的bug

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -5,7 +5,9 @@ import java.util.Collection;
 import java.util.HashSet;
 
 public class CountingSet extends HashSet<Object> {
-    /** 统计"有史以来"向该集合中添加过的元素个数 */
+    /**
+     * 统计"有史以来"向该集合中添加过的元素个数
+     */
     private int count = 0;
 
     @Override
@@ -14,11 +16,12 @@ public class CountingSet extends HashSet<Object> {
         return super.add(obj);
     }
 
-    @Override
+/*    @Override
     public boolean addAll(Collection c) {
+        //c長度是3，又加了一次addAlld 的長度4 則是7
         count += c.size();
         return super.addAll(c);
-    }
+    }*/
 
     public int getCount() {
         return count;
@@ -29,7 +32,9 @@ public class CountingSet extends HashSet<Object> {
     // 请尝试修复此问题
     public static void main(String[] args) {
         CountingSet countingSet = new CountingSet();
+        //創建一個對象添加
         countingSet.add(new Object());
+        //添加三個對象
         countingSet.addAll(Arrays.asList(1, 2, 3));
 
         System.out.println(countingSet.getCount());
