98fb73da93f6edb8d0ab01fa23aab27edec1f8f1
niuweizhe <niu853273494@qq.com>
2021-04-18 11:23:41 +0800

    change addAll to add

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -17,7 +17,7 @@ public class CountingSet extends HashSet<Object> {
     @Override
     public boolean addAll(Collection c) {
         count += c.size();
-        return super.addAll(c);
+        return super.add(c);
     }
 
     public int getCount() {
