de96d0be26ccdacea6362e9e24e5b916fd1246b5
reallEz <jingqing.xj@outlook.com>
2021-07-13 10:11:08 +0800

    当你使用组合时，你怎么知道你需要重写哪些方法呢

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -1,23 +1,102 @@
 package com.github.hcsp.inheritance;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
-public class CountingSet extends HashSet<Object> {
+public class CountingSet {
     /** 统计"有史以来"向该集合中添加过的元素个数 */
     private int count = 0;
+    
+    private HashSet<Object> hashSet = new HashSet<>();
 
-    @Override
-    public boolean add(Object obj) {
-        count++;
-        return super.add(obj);
+    public Iterator<Object> iterator() {
+        return hashSet.iterator();
     }
 
-    @Override
+    public int size() {
+        return hashSet.size();
+    }
+
+    public boolean isEmpty() {
+        return hashSet.isEmpty();
+    }
+
+    public boolean contains(Object o) {
+        return hashSet.contains(o);
+    }
+
+    public boolean remove(Object o) {
+        return hashSet.remove(o);
+    }
+
+    public void clear() {
+        hashSet.clear();
+    }
+
+    public Object clone() {
+        return hashSet.clone();
+    }
+
+    public Spliterator<Object> spliterator() {
+        return hashSet.spliterator();
+    }
+
+    public Object[] toArray() {
+        return hashSet.toArray();
+    }
+
+    public <T> T[] toArray(T[] a) {
+        return hashSet.toArray(a);
+    }
+
+    public boolean equals(Object o) {
+        return hashSet.equals(o);
+    }
+
+    public int hashCode() {
+        return hashSet.hashCode();
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        return hashSet.removeAll(c);
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        return hashSet.containsAll(c);
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        return hashSet.retainAll(c);
+    }
+
+    public String toString() {
+        return hashSet.toString();
+    }
+
+    public <T> T[] toArray(IntFunction<T[]> generator) {
+        return null;
+    }
+
+    public Stream<Object> stream() {
+        return null;
+    }
+
+    public Stream<Object> parallelStream() {
+        return null;
+    }
+    
+    public boolean add(Object obj) {
+        count++;
+        return hashSet.add(obj);
+    }
+
+    
     public boolean addAll(Collection c) {
         count += c.size();
-        return super.addAll(c);
+        return hashSet.addAll(c);
     }
 
     public int getCount() {
