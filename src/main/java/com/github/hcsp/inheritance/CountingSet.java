960ee396e1a79b7a6a3f183c5dda0fc944f8baba
GJC <599953528@qq.com>
2024-09-12 19:02:06 +0800

    任务3

--- src/main/java/com/github/hcsp/inheritance/CountingSet.java
+++ src/main/java/com/github/hcsp/inheritance/CountingSet.java
@@ -4,42 +4,47 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 
-public class CountingSet extends HashSet<Object> {
+public class CountingSet  {
     /** 统计"有史以来"向该集合中添加过的元素个数 */
+    HashSet<Object> set =new HashSet<>();
     private int count = 0;
-    private final HashSet<Object> seen = new HashSet<>();
 
-    @Override
+//    @Override
     public boolean add(Object obj) {
-        // 如果对象之前没有被添加过
-        if (seen.add(obj)) {
-            count++;
-        }
-        return super.add(obj);
+        count++;
+        return set.add(obj);
     }
 
-    @Override
+
     public boolean addAll(Collection c) {
-        boolean changed = false;
-        for (Object obj : c) {
-            if (seen.add(obj)) {
-                count++;
-                changed = true;
-            }
-        }
-        return changed || super.addAll(c);
+        count += c.size();
+        return set.addAll(c);
     }
 
     public int getCount() {
         return count;
     }
 
-    // 测试代码
+    public boolean remove(Object obj) {
+        return set.remove(obj);
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        return set.removeAll(c);
+    }
+
+    public int size() {
+        return set.size();
+    }
+
+    // 我们希望创建一个Set，能够统计"有史以来"添加到其中去的元素个数
+    // 但是，现在结果明显不对
+    // 请尝试修复此问题
     public static void main(String[] args) {
         CountingSet countingSet = new CountingSet();
         countingSet.add(new Object());
         countingSet.addAll(Arrays.asList(1, 2, 3));
 
-        System.out.println(countingSet.getCount()); // 应该输出4
+        System.out.println(countingSet.getCount());
     }
 }
